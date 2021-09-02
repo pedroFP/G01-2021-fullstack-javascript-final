2d15d6dc6bbf0cc0801a18de30f3c1dfb7e6c555
Pedro <pedro@vinkodigital.com>
2021-09-01 20:07:38 -0400

    initial commit

---
+++ src/firebase/config.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyAJmu5F73IZ5S58REQlSG01q6bvTyEZdHs',
+  authDomain: 'boolean-proyect.firebaseapp.com',
+  projectId: 'boolean-proyect',
+  storageBucket: 'boolean-proyect.appspot.com',
+  messagingSenderId: '98075749452',
+  appId: '1:98075749452:web:e7e89d70dca6d43d2498e9',
+  measurementId: 'G-4VVKS2WRBT'
+}
---
+++ src/views/Products.vue
@@ -0,0 +1,11 @@
+<template>
+  <v-main>
+    <h1>Productos</h1>
+  </v-main>
+</template>
+
+<script>
+export default {
+
+}
+</script>
